1ea19bd83270b69744350dd2893288c06b09e4a5
Winfried <winfried.loetzsch@student.hpi.de>
2020-07-01 17:24:29 +0200

    gpu auto assign

--- test_app/mnist_example.py
+++ test_app/mnist_example.py
@@ -7,55 +7,66 @@ from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.optimizers import Adadelta
 from tensorflow_core.python.keras.utils.np_utils import to_categorical
 
+from assign_gpu import aquire_free_gpus
 from tracker_callback import TrackerCallback
 
-batch_size = 128
-num_classes = 10
-epochs = 12
 
-# input image dimensions
-img_rows, img_cols = 28, 28
+def run_mnist():
+    batch_size = 128
+    num_classes = 10
+    epochs = 12
+    gpu = False
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    if gpu:
+        aquire_free_gpus()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    # input image dimensions
+    img_rows, img_cols = 28, 28
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# convert class vectors to binary class matrices
-y_train = to_categorical(y_train, num_classes)
-y_test = to_categorical(y_test, num_classes)
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
 
-model.compile(loss=categorical_crossentropy,
-              optimizer=Adadelta(),
-              metrics=['accuracy'])
+    # convert class vectors to binary class matrices
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
 
-model.fit(x_train, y_train,
-          callbacks=[TrackerCallback()],
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=categorical_crossentropy,
+                  optimizer=Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+              callbacks=[TrackerCallback()],
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+
+
+if __name__ == "__main__":
+    run_mnist()
